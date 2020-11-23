329544e00823a16b8244acabd538fa3e09a4211b
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-11-23 17:11:38 +0900

    react fetch from go

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const resResult = await fetch('http://localhost:8080/api/fgi/')
+            resResult
+                .json()
+                .then((resResult) => setData(resResult))
+                .catch(() => null)
+        }
+        fetchData()
+    }, []);
+
+    console.log(data)
     return (
         <div>
             client
