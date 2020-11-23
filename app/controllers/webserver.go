2e41c19cfd07fbd8c28015be05e664d6671f0fee
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-11-23 10:45:30 +0900

    create fgi api

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"index-indicator-apis/app/models"
 	"index-indicator-apis/config"
@@ -20,8 +24,51 @@ func viewFgiHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type JSONError struct {
+	Error string `json:"error"`
+	Code  int    `json:"code"`
+}
+
+func APIError(w http.ResponseWriter, errMessage string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	jsonError, err := json.Marshal(JSONError{Error: errMessage, Code: code})
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(jsonError)
+}
+
+var apiValidPath = regexp.MustCompile("^/api/fgi/$")
+
+func apiMakeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := apiValidPath.FindStringSubmatch(r.URL.Path)
+		if len(m) == 0 {
+			APIError(w, "Not found", http.StatusNotFound)
+		}
+		fn(w, r)
+	}
+}
+
+func apiFgiHandler(w http.ResponseWriter, r *http.Request) {
+	strLimit := r.URL.Query().Get("limit")
+	limit, err := strconv.Atoi(strLimit)
+	if strLimit == "" || err != nil || limit < 0 || limit > 100 {
+		limit = 100
+	}
+	fgi := models.GetLatestFgi()
+	js, err := json.Marshal(fgi)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
 // StartWebServer webserver立ち上げ
 func StartWebServer() error {
+	http.HandleFunc("/api/fgi/", apiMakeHandler(apiFgiHandler))
 	http.HandleFunc("/chart/", viewFgiHandler)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
