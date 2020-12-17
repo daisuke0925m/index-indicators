140ca1fe79a42d74e23f73db8c21697f7589fd80
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-12-17 19:21:31 +0900

    create BlockQuote-comp

--- client/src/components/Fgi/FgiDes.jsx
+++ client/src/components/Fgi/FgiDes.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import BlockQuote from "../UiKits/BlockQuote";
 
 const ComponentName = () => {
     return (
-        <div>
-            Investors are driven by two emotions: fear and greed. Too much fear can sink stocks well below where they should be. When investors get greedy, they can bid up stock prices way too far.<br />
+        <BlockQuote>
+            <div>
+                Investors are driven by two emotions: fear and greed. Too much fear can sink stocks well below where they should be. When investors get greedy, they can bid up stock prices way too far.<br />
             So what emotion is driving the market now? CNNMoney's Fear & Greed index makes it clear.<br />
             We look at 7 indicators:<br />
             •Stock Price Momentum: The S&P 500 (SPX) versus its 125-day moving average<br />
@@ -17,10 +19,11 @@ const ComponentName = () => {
             Then we put all the indicators together - equally weighted - for a final index reading.<br />
             When the S&P 500 (SPX) plummeted to a three-year low on Sept. 17, 2008 - the height of the financial crisis -- the Fear and Greed index sank to 12. The index gained some ground to 28 before stocks finally bottomed out on March 9, 2009 and the latest bull market began.<br />
             Most recently, in the first quarter of 2012, stocks staged their best run in decades, and the index showed pure greed.<br />
-            <p style={{ textAlign: 'right' }}>
-                from cnn Money
+                <p style={{ textAlign: 'right' }}>
+                    from cnn Money
             </p>
-        </div>
+            </div>
+        </BlockQuote>
     )
 };
 
