44a131794f58d29997dadebc73cdc51b97ad3e2f
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-12-19 11:13:33 +0900

    migrationを一括管理

--- server/mysql/mysql.go
+++ server/mysql/mysql.go
@@ -1,6 +1,8 @@
 package mysql
 
 import (
+	. "index-indicator-apis/server/app/entity"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
@@ -10,10 +12,21 @@ func SQLConnect() (database *gorm.DB, err error) {
 	DBMS := "mysql"
 	USER := "iia"
 	PASS := "iia"
-	PROTOCOL := "tcp(mysql_container:3306)"
+	PROTOCOL := "tcp(localhost:3306)"
 	DBNAME := "index_indicator_apis"
 
 	CONNECT := (USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo")
 
 	return gorm.Open(DBMS, CONNECT)
+}
+
+//AutoMigrate マイグレーション
+func AutoMigrate() {
+	var err error
+	db, err := SQLConnect()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	db.AutoMigrate(&Fgi{})
 }
