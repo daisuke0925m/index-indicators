663a45422198619ce14dd8217c008dc3c7cc0c17
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-12-19 09:52:26 +0900

    print削除

--- server/app/controllers/streamdata.go
+++ server/app/controllers/streamdata.go
@@ -1,12 +1,11 @@
 package controllers
 
 import (
-	"fmt"
 	"index-indicator-apis/server/app/models"
 )
 
 // StreamIngestionData api保存を定期実行
 func StreamIngestionData() {
-	fmt.Println(models.CreateNewFgis()) //TODO 定期実行(米株市場毎営業日の前後)
+	models.CreateNewFgis() //TODO 定期実行(米株市場毎営業日の前後)
 
 }
