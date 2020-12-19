38ea7334fb0e538366d8f1942d8c9a30e366cf85
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-12-19 15:31:32 +0900

    apiのinsertをgorutineで処理

--- server/app/controllers/streamdata.go
+++ server/app/controllers/streamdata.go
@@ -6,6 +6,6 @@ import (
 
 // StreamIngestionData api保存を定期実行
 func StreamIngestionData() {
-	models.CreateNewFgis() //TODO 定期実行(米株市場毎営業日の前後)
+	go models.CreateNewFgis() //TODO 定期実行(米株市場毎営業日の前後)
 
 }
