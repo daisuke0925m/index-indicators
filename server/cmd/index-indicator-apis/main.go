7ad33b4dd1343f1aed8e9064c30133436e311683
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-12-19 11:24:17 +0900

    起動時のマイグレーションを実行

--- server/cmd/index-indicator-apis/main.go
+++ server/cmd/index-indicator-apis/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"index-indicator-apis/server/app/controllers"
+	"index-indicator-apis/server/mysql"
 )
 
 func main() {
+	mysql.AutoMigrate()
 	controllers.StreamIngestionData()
 	controllers.StartWebServer()
 }
