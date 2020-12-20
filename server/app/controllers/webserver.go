34464e8c89dc2a59de57288be05e3e5fc1c8f1d6
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-12-20 12:06:35 +0900

    handlerの雛形作成

--- server/app/controllers/webserver.go
+++ server/app/controllers/webserver.go
@@ -62,11 +62,23 @@ func apiFgiHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
+func apiSignupHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "signup")
+	fmt.Println("signup 関数実行")
+}
+
+func apiLoginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "login")
+	fmt.Println("login 関数実行")
+}
+
 // StartWebServer webserver立ち上げ
 func StartWebServer() error {
 	fmt.Println("connecting...")
-	http.HandleFunc("/api/fgi/", apiMakeHandler(apiFgiHandler))
 	http.HandleFunc("/", viewFgiHandler)
+	http.HandleFunc("/api/fgi/", apiMakeHandler(apiFgiHandler))
+	http.HandleFunc("/api/signup", apiMakeHandler(apiSignupHandler))
+	http.HandleFunc("/api/login", apiMakeHandler(apiLoginHandler))
 	fmt.Printf("connected port :%d\n", config.Config.Port)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
