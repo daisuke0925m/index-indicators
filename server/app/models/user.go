c9672b7e5deb38b3af94a92f08b2c534d3c223a3
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-12-29 11:53:46 +0900

    urlベースで検索するメソッド

--- server/app/models/user.go
+++ server/app/models/user.go
@@ -3,6 +3,9 @@ package models
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"path"
+	"strconv"
 	"time"
 
 	"index-indicator-apis/server/app/entity"
@@ -36,4 +39,24 @@ func CreateUser(user entity.User) (err error) {
 	fmt.Printf("%v\n", user)
 	fmt.Println("finish! created a user")
 	return
+}
+
+// FindUserByID idからuserを検索
+func FindUserByID(r *http.Request) (entity.User, error) {
+	var user entity.User
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	db, err := db.SQLConnect()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	if err := db.Where("id = ?", id).First(&user).Error; err != nil {
+		fmt.Println("error!")
+		return user, err
+	}
+	return user, nil
 }
