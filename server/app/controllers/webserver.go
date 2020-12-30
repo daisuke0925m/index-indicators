6532886272971a0348460ae7c39be74dc5bbdaab
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-12-30 15:58:34 +0900

    自前のメソッドをgorillaのメソッドに変更

--- server/app/controllers/webserver.go
+++ server/app/controllers/webserver.go
@@ -50,19 +50,7 @@ func tokenVerifyMiddleWare(fn func(http.ResponseWriter, *http.Request)) http.Han
 	})
 }
 
-func checkHTTPMethod(method string, w http.ResponseWriter, r *http.Request) int {
-	if r.Method != method {
-		return http.StatusMethodNotAllowed
-	}
-	return http.StatusOK
-}
-
 func apiFgiHandler(w http.ResponseWriter, r *http.Request) {
-	if result := checkHTTPMethod("GET", w, r); result != http.StatusOK {
-		apiError(w, "bad request", result)
-		return
-	}
-
 	strLimit := r.URL.Query().Get("limit")
 	limit, err := strconv.Atoi(strLimit)
 	if strLimit == "" || err != nil || limit < 0 || limit > 100 {
@@ -79,11 +67,6 @@ func apiFgiHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func signupHandler(w http.ResponseWriter, r *http.Request) {
-	if result := checkHTTPMethod("POST", w, r); result != http.StatusOK {
-		apiError(w, "bad request", result)
-		return
-	}
-
 	var user entity.User
 	json.NewDecoder(r.Body).Decode(&user)
 
@@ -109,11 +92,6 @@ func signupHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func userDeleteHandler(w http.ResponseWriter, r *http.Request) {
-	if result := checkHTTPMethod("DELETE", w, r); result != http.StatusOK {
-		apiError(w, "bad request", result)
-		return
-	}
-
 	foundUser, err := models.FindUserByID(r)
 	if err != nil {
 		apiError(w, err.Error(), http.StatusInternalServerError)
@@ -140,10 +118,6 @@ func userDeleteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // func userUpdateHandler(w http.ResponseWriter, r *http.Request) {
-// 	// if result := checkHTTPMethod("PUT", w, r); result != http.StatusOK {
-// 	// 	apiError(w, "bad request", result)
-// 	// 	return
-// 	// }
 
 // 	// foundUser, err := models.FindUserByID(r)
 // 	// if err != nil {
@@ -167,11 +141,6 @@ func userDeleteHandler(w http.ResponseWriter, r *http.Request) {
 // }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	if result := checkHTTPMethod("POST", w, r); result != http.StatusOK {
-		apiError(w, "bad request", result)
-		return
-	}
-
 	var user entity.User
 	json.NewDecoder(r.Body).Decode(&user)
 
@@ -211,11 +180,6 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
-	if result := checkHTTPMethod("POST", w, r); result != http.StatusOK {
-		apiError(w, "bad request", result)
-		return
-	}
-
 	accessDetails, err := models.ExtractTokenMetadata(r)
 	if err != nil {
 		apiError(w, "not found", http.StatusNotFound)
@@ -232,11 +196,6 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func refreshTokenHandler(w http.ResponseWriter, r *http.Request) {
-	if result := checkHTTPMethod("POST", w, r); result != http.StatusOK {
-		apiError(w, "bad request", result)
-		return
-	}
-
 	mapToken := map[string]string{}
 	json.NewDecoder(r.Body).Decode(&mapToken)
 	refreshToken := mapToken["refresh_token"]
@@ -257,13 +216,13 @@ func StartWebServer() error {
 	http.Handle("/", r)
 
 	fmt.Println("connecting...")
-	r.HandleFunc("/fgi", tokenVerifyMiddleWare(apiFgiHandler))
-	r.HandleFunc("/signup", signupHandler)
-	r.HandleFunc("/login", loginHandler)
-	r.HandleFunc("/refresh_token", refreshTokenHandler)
-	r.HandleFunc("/logout", tokenVerifyMiddleWare(logoutHandler))
-	r.HandleFunc("/users/", userDeleteHandler)
-	// r.HandleFunc("/users/", userUpdateHandler)
+	r.HandleFunc("/fgi", tokenVerifyMiddleWare(apiFgiHandler)).Methods("GET")
+	r.HandleFunc("/login", loginHandler).Methods("POST")
+	r.HandleFunc("/refresh_token", refreshTokenHandler).Methods("POST")
+	r.HandleFunc("/logout", tokenVerifyMiddleWare(logoutHandler)).Methods("POST")
+	r.HandleFunc("/signup", signupHandler).Methods("POST")
+	r.HandleFunc("/users", userDeleteHandler).Methods("DELETE")
+	// r.HandleFunc("/users/", userUpdateHandler).Methods("GET")
 	fmt.Printf("connected port :%d\n", config.Config.Port)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
