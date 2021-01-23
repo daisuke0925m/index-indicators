0fea5e44706af0fca24223c6bd0f326e24426bd1
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-24 08:37:27 +0900

    lint

--- client/src/components/UiKits/BlockQuote.jsx
+++ client/src/components/UiKits/BlockQuote.jsx
@@ -1,35 +1,32 @@
-import React from "react";
+import React from 'react';
 import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    blockquote: {
-        position: 'relative',
-        padding: '35px 15px 10px 15px',
-        boxSizing: 'border-box',
-        fontStyle: 'italic',
-        background: '#f5f5f5',
-        color: '#777777',
-        borderLeft: '4px solid #447B64',
-        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.14)',
-    },
-    iconRoot: {
-        transform: 'scale(-1, -1)',
-    }
+  blockquote: {
+    position: 'relative',
+    padding: '35px 15px 10px 15px',
+    boxSizing: 'border-box',
+    fontStyle: 'italic',
+    background: '#f5f5f5',
+    color: '#777777',
+    borderLeft: '4px solid #447B64',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.14)',
+  },
+  iconRoot: {
+    transform: 'scale(-1, -1)',
+  },
 }));
 
 const BlockQuote = (props) => {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    return (
-        <div className={classes.blockquote}>
-            <FormatQuoteIcon
-                className={classes.iconRoot}
-                color='primary'
-            />
-            {props.children}
-        </div>
-    )
+  return (
+    <div className={classes.blockquote}>
+      <FormatQuoteIcon className={classes.iconRoot} color="primary" />
+      {props.children}
+    </div>
+  );
 };
 
-export default BlockQuote
+export default BlockQuote;
