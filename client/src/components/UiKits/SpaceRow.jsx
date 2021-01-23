2bb2e891cd2b37fa5dd356956155aef5a144f0e0
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-24 08:35:50 +0900

    lint

--- client/src/components/UiKits/SpaceRow.jsx
+++ client/src/components/UiKits/SpaceRow.jsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const SpaceRow = (props) => {
-    return (
-        <div style={{ margin: props.height }} />
-    )
+  return <div style={{ margin: props.height }} />;
 };
 
-export default SpaceRow
+export default SpaceRow;
