061c4a7baa0c45eba373746b99496df9264fe619
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-24 08:40:41 +0900

    lint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,27 @@
-import React from 'react'
+import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
-import './style/index.css'
-import Main from './components/Main/Main'
-import Header from './components/Header/Header'
+import './style/index.css';
+import Main from './components/Main/Main';
+import Header from './components/Header/Header';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#447B64',
-        },
+  palette: {
+    primary: {
+      main: '#447B64',
     },
+  },
 });
 
-
 const App = () => {
-
-    return (
-        <ThemeProvider theme={theme}>
-            <div>
-                <Header />
-                <Main />
-            </div>
-        </ThemeProvider>
-    );
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <Header />
+        <Main />
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
