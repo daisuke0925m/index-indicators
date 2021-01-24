1f84093998a57cdb05ec47d8f625ada9d9f6d907
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-24 09:26:26 +0900

    test

--- client/src/components/Fgi/Fgi.jsx
+++ client/src/components/Fgi/Fgi.jsx
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Fgi = () => {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get('/fgi');
-        setData(response.data);
-      } catch (error) {
-        console.log(error);
-        setData([]);
-      }
-    }
-    fetchData();
-  }, []);
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get('/fgi');
+                setData(response.data);
+            } catch (error) {
+                console.log(error);
+                setData([]);
+            }
+        }
+        fetchData();
+    }, []);
 
-  return <div>{data.length ? data.map((d, i) => <div key={i}>{d.now_value}</div>) : 'loading'}</div>;
+    return <div>{data.length ? data.map((d, i) => <div key={i}>{d.now_value}</div>) : 'loading'}</div>;
 };
+
+
 
 export default Fgi;
