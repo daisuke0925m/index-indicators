8cfc43d3a1eafe3afff54fc12197841757d7fdc4
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-25 14:38:50 +0900

    日付取得

--- client/src/components/Skew/Skew.jsx
+++ client/src/components/Skew/Skew.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import StockChart from '../Chart/StockChart';
+import { tickerDateParse } from '../Functions/functions';
 
 const Skew = () => {
     const [skews, setFgis] = useState([]);
+    const today = new Date();
+    const parsedToday = tickerDateParse(today, 0);
+    const parsed1mAgo = tickerDateParse(today, -50);
 
     useEffect(() => {
         async function fetchSkews() {
             try {
-                const response = await axios.get('/ticker?symbol=^skew&start=2021-01-10&end=2021-01-25');
+                const response = await axios.get(`/ticker?symbol=^skew&start=${parsed1mAgo}&end=${parsedToday}`);
                 setFgis(response.data);
             } catch (error) {
                 console.log(error);
