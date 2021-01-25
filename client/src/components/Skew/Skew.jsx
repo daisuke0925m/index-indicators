5918f9f6af7106bf9a582d87fdcb06a2d7269763
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-25 14:07:27 +0900

    get skews

--- client/src/components/Skew/Skew.jsx
+++ client/src/components/Skew/Skew.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import StockChart from '../Chart/StockChart';
 
 const Skew = () => {
+    const [skews, setFgis] = useState([]);
+
+    useEffect(() => {
+        async function fetchSkews() {
+            try {
+                const response = await axios.get('/ticker?symbol=^skew&start=2021-01-10&end=2021-01-25');
+                setFgis(response.data);
+            } catch (error) {
+                console.log(error);
+                setFgis([]);
+            }
+        }
+        fetchSkews();
+    }, []);
+    console.log(skews);
+
     return (
         <>
             <StockChart />
