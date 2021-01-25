0e9f2db27d82816b82c8c41d0b1173bdef8aa5f0
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-25 17:27:32 +0900

    symbol存在チェック

--- server/app/models/ticker.go
+++ server/app/models/ticker.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"index-indicator-apis/server/app/entity"
 	"index-indicator-apis/server/db"
 	"time"
@@ -14,12 +15,13 @@ func dateToString(d time.Time) string {
 	return dString
 }
 
-func createTickerRow(symbol string, date time.Time, open, high, low, close, volume float64) (err error) {
+func createTickerRow(symbol string, date time.Time, open, high, low, close, volume float64) error {
 	db, err := db.SQLConnect()
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 	defer db.Close()
+
 	tickerRow := &entity.Ticker{
 		Symbol:    symbol,
 		Date:      date,
@@ -30,11 +32,26 @@ func createTickerRow(symbol string, date time.Time, open, high, low, close, volu
 		Volume:    volume,
 		CreatedAt: time.Now(),
 	}
-
 	if err := db.Create(&tickerRow).Error; err != nil {
 		return err
 	}
+
 	return nil
+}
+
+func checkSymbolExist(symbol string) bool {
+	db, err := db.SQLConnect()
+	if err != nil {
+		return false
+	}
+	defer db.Close()
+
+	var ticker entity.Ticker
+	if err := db.Where("symbol = ?", symbol).First(&ticker).Error; err != nil {
+		return false
+	}
+
+	return true
 }
 
 // SaveTickers save tickers data
@@ -42,7 +59,8 @@ func SaveTickers() (err error) {
 	// symbols := []string{"spxl"}
 	symbols := []string{"spxl", "^skew", "tlt", "gld", "gldm", "spy"}
 	today := time.Now()
-	twoYAgo := today.AddDate(-2, 0, 0)
+	twoYAgo := today.AddDate(0, 0, -2)
+	// twoYAgo := today.AddDate(-2, 0, 0)
 
 	for _, symbol := range symbols {
 		// save 2years data
@@ -51,11 +69,15 @@ func SaveTickers() (err error) {
 			return err
 		}
 
-		dataLength := len(tickerData.Open)
-		for i := 0; i < dataLength; i++ {
-			err := createTickerRow(symbol, tickerData.Date[i], tickerData.Open[i], tickerData.High[i], tickerData.Low[i], tickerData.Close[i], tickerData.Volume[i])
-			if err != nil {
-				return err
+		if checkSymbolExist(symbol) {
+			fmt.Println(symbol)
+		} else {
+			dataLength := len(tickerData.Open)
+			for i := 0; i < dataLength; i++ {
+				err := createTickerRow(symbol, tickerData.Date[i], tickerData.Open[i], tickerData.High[i], tickerData.Low[i], tickerData.Close[i], tickerData.Volume[i])
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
