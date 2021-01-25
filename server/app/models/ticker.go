1f7d700bd02b353ea969c770992faeed0a64c845
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-01-25 18:30:00 +0900

    dateをチェックし1レコード更新

--- server/app/models/ticker.go
+++ server/app/models/ticker.go
@@ -54,12 +54,43 @@ func checkSymbolExist(symbol string) bool {
 	return true
 }
 
+func checkLatestRecord(symbol string, date time.Time) bool {
+	db, err := db.SQLConnect()
+	if err != nil {
+		return false
+	}
+	defer db.Close()
+
+	var ticker entity.Ticker
+	if err := db.Where("symbol = ? AND date = ?", symbol, date).First(&ticker).Error; err != nil {
+		return false
+	}
+
+	return true
+}
+
+func deleteLastRecord(symbol string, date time.Time) error {
+	db, err := db.SQLConnect()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	var ticker entity.Ticker
+	if err := db.Where("symbol = ? AND date = ?", symbol, date).Delete(&ticker).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // SaveTickers save tickers data
 func SaveTickers() (err error) {
 	// symbols := []string{"spxl"}
 	symbols := []string{"spxl", "^skew", "tlt", "gld", "gldm", "spy"}
 	today := time.Now()
-	twoYAgo := today.AddDate(0, 0, -2)
+	twoYAgo := today.AddDate(0, 0, -7)
+	// d := today.AddDate(0, 0, -3)
 	// twoYAgo := today.AddDate(-2, 0, 0)
 
 	for _, symbol := range symbols {
@@ -69,20 +100,38 @@ func SaveTickers() (err error) {
 			return err
 		}
 
+		dataLength := len(tickerData.Open)
+		len := dataLength - 1
+		fmt.Println(tickerData)
+
+		// elseの場合のみ全ての結果をinsertする
 		if checkSymbolExist(symbol) {
-			fmt.Println(symbol)
+			lastRecordDate := tickerData.Date[len]
+
+			// dbにある最新のdateと取得したdateが一致した場合は処理を抜ける
+			if !checkLatestRecord(symbol, lastRecordDate) {
+				// 一致しなかった場合は一番古い日付のレコードを削除し最新のレコードをinsert
+				err := deleteLastRecord(symbol, tickerData.Date[0])
+				if err != nil {
+					return err
+				}
+				err = createTickerRow(symbol, tickerData.Date[len], tickerData.Open[len], tickerData.High[len], tickerData.Low[len], tickerData.Close[len], tickerData.Volume[len])
+				if err != nil {
+					return err
+				}
+			}
+
 		} else {
-			dataLength := len(tickerData.Open)
+
 			for i := 0; i < dataLength; i++ {
 				err := createTickerRow(symbol, tickerData.Date[i], tickerData.Open[i], tickerData.High[i], tickerData.Low[i], tickerData.Close[i], tickerData.Volume[i])
 				if err != nil {
 					return err
 				}
 			}
+
 		}
 	}
 
-	// fmt.Println(dateToString(today))
-	// fmt.Println(dateToString(twoYAgo))
-	return err
+	return nil
 }
