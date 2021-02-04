16d729048891ad24554ffc02f351838d84125f99
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-02-04 09:23:23 +0900

    getuseName

--- client/src/redux/users/selectors.js
+++ client/src/redux/users/selectors.js
@@ -5,3 +5,5 @@ const usersSelector = (state) => state.users;
 export const getSignedIn = createSelector([usersSelector], (state) => state.isSignedIn);
 
 export const getUserID = createSelector([usersSelector], (state) => state.userID);
+
+export const getUserName = createSelector([usersSelector], (state) => state.userName);
