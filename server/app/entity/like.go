b12e045bbecb98f3db72f82130aec5a0efdd7bb3
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-02-05 08:41:12 +0900

    アソシエーション修正

--- server/app/entity/like.go
+++ server/app/entity/like.go
@@ -6,7 +6,7 @@ import "time"
 type Like struct {
 	ID        int       `json:"id,omitempty" gorm:"primaryKey,unique"`
 	UserID    int       `json:"user_id,omitempty"`
-	User      User      `gorm:"foreignkey:UserID;association_foreignkey:Refer"`
+	User      User      `gorm:"foreignkey:UserID"`
 	Symbol    string    `json:"symbol,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
 }
