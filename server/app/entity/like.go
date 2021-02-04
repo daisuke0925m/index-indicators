b6ad12e8390904bc97dd4068df866571ac568ffc
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-02-04 17:11:47 +0900

    like構造作成

---
+++ server/app/entity/like.go
@@ -0,0 +1,12 @@
+package entity
+
+import "time"
+
+// Like type Like
+type Like struct {
+	ID        int       `json:"id,omitempty" gorm:"primaryKey,unique"`
+	UserID    int       `json:"user_id,omitempty"`
+	User      User      `gorm:"foreignkey:UserID;association_foreignkey:Refer"`
+	Symbol    string    `json:"symbol,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+}
