cda7a3f01dfcdbe4d74bf85493a45e41cbc63538
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-02-04 17:12:25 +0900

    不要なメソッド削除

--- server/db/mysql.go
+++ server/db/mysql.go
@@ -1,8 +1,6 @@
 package db
 
 import (
-	"fmt"
-	"index-indicator-apis/server/app/entity"
 	"os"
 
 	// Register for gorm
@@ -21,16 +19,4 @@ func SQLConnect() (database *gorm.DB, err error) {
 	CONNECT := (USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo")
 
 	return gorm.Open(DBMS, CONNECT)
-}
-
-//AutoMigrate マイグレーション
-func AutoMigrate() {
-	fmt.Println("migrating database...")
-	db, err := SQLConnect()
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-	db.AutoMigrate(&entity.Fgi{}, &entity.User{})
-	fmt.Println("finish migrate!")
 }
