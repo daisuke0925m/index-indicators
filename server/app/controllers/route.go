fddf482702c5204d6316cae02fc3b66279c35c1f
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-02-05 11:10:34 +0900

    ルーティング

--- server/app/controllers/route.go
+++ server/app/controllers/route.go
@@ -7,10 +7,14 @@ import (
 // Route return API routing
 func Route(app *App) *mux.Router {
 	r := mux.NewRouter()
+	// user
 	r.HandleFunc("/users/{id:[0-9]+}", app.userGetHandler).Methods("GET")
 	r.HandleFunc("/users", app.signupHandler).Methods("POST", "OPTIONS")
 	r.HandleFunc("/users/{id:[0-9]+}", app.userDeleteHandler).Methods("DELETE", "OPTIONS")
 	r.HandleFunc("/users/{id:[0-9]+}", app.userUpdateHandler).Methods("PUT")
+	// like
+	r.HandleFunc("/users/{id:[0-9]+}/likes", app.likePostHandler).Methods("POST")
+	// r.HandleFunc("/users/{id:[0-9]+}/likes/{id:[0-9]+}", app.tokenVerifyMiddleWare(app.likeDeleteHandler)).Methods("DELETE")
 	// auth
 	r.HandleFunc("/login", app.loginHandler).Methods("POST", "OPTIONS")
 	r.HandleFunc("/logout", app.tokenVerifyMiddleWare(app.logoutHandler)).Methods("POST")
